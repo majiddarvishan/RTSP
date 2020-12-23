a11627a27b884b3bec2a94ea2ef5eb248b18401d
Majid Darvishan <majiddarvishan@outlook.com>
2020-12-23 18:57:18 +0330

    Get URLfrom user

--- tests/rtspclient.cpp
+++ tests/rtspclient.cpp
@@ -3,6 +3,8 @@
 #include <conio.h>
 #endif
 
+#include <iostream>
+
 //#define RTSPCLIENT_DLL
 
 #ifdef LINUX
@@ -61,6 +63,11 @@ static void frameHandlerFunc(void *arg, RTP_FRAME_TYPE frame_type, int64_t times
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        std::cout << "Please enter URL\n";
+        return 0;
+    }
 #ifdef _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
@@ -71,7 +78,8 @@ int main(int argc, char *argv[])
 #else
 	RTSPCommonEnv::SetDebugFlag(DEBUG_FLAG_RTSP);
 #endif
-	char *strURL = "rtsp://222.96.113.48:4554/AVStream1_2";
+	//char *strURL = "rtsp://222.96.113.48:4554/AVStream1_2";
+    char* strURL =argv[1];
 
 	fp_dump = fopen("video.264", "wb");
 
